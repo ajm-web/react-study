854ae79ff25a0dee0c351b1a606446a5e6d44fc3
uyaponz <uyaponz@gmail.com>
2016-11-25 06:02:54 +0000

    Reduxのサンプルを追加。

--- src/js/calculator/NumberButtonComponent.tsx
+++ src/js/calculator/NumberButtonComponent.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import * as NumberInputActions from "./NumberInputActions";
+import * as NumberInputStore from "./NumberInputStore";
 import RaisedButton from "material-ui/RaisedButton";
 
 export interface NumberButton {
@@ -9,15 +11,15 @@ export default class NumberButtonComponent extends React.Component<NumberButton,
 constructor(props: NumberButton) {
     super(props);
     }
-    Test(){
-        alert("test");
+    Test(buttonNumber: number){
+        NumberInputStore.instance.dispatch({ type: NumberInputActions.ActionType.Push, payload: String(buttonNumber) });
     }
     render() {
         const style = {
             height: 50
         };
         return (
-            <RaisedButton label={this.props.buttonNumber} fullWidth={true} onClick={this.Test} style={style}/>
+            <RaisedButton label={this.props.buttonNumber} fullWidth={true} onClick={() => this.Test(this.props.buttonNumber)} style={style}/>
         );
     }
 }
